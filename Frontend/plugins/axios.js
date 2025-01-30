a02925733a370020a75eae7b5d28be89e742897c
weam <weamibrahim46@gmail.com>
2025-01-30 16:29:02 +0200

    update url to connect with backend

--- Frontend/plugins/axios.js
+++ Frontend/plugins/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default defineNuxtPlugin(() => {
 
   const axiosInstance = axios.create({
-    baseURL: "http://localhost:7000/api",
+    baseURL: "https://gym-managment-kappa.vercel.app/api",
     withCredentials: true,
     headers: {
       "Content-Type": "application/json",
